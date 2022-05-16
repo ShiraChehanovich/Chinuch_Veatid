56a6f7754243540af13acbdd14c777896a8047ae
Hodaya <kalihodaya@gmail.com>
2022-05-16 23:02:43 +0300

    Finish menu

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -7,25 +7,28 @@ import './Main.css'
 
 export default function Main() {
   return (
-      <div width="100%">
+      <div>
+         <div className='topright'>
+                <img  src="https://chv.org.il/wp-content/uploads/2019/06/%D7%9C%D7%95%D7%92%D7%95-%D7%9E%D7%A9%D7%95%D7%9C%D7%91_2-1024x251.png" width="40%"></img>
+                </div>
         <div className='flex-container'>
             <div className='location'>
                  <DashboardMenu />
             </div>
             <div className='right-aligned'>
-                <div>
+                {/* <div className='topright'>
                 <img  src="https://chv.org.il/wp-content/uploads/2019/06/%D7%9C%D7%95%D7%92%D7%95-%D7%9E%D7%A9%D7%95%D7%9C%D7%91_2-1024x251.png" width="40%"></img>
-                </div>
+                </div> */}
             </div>
         </div>
         <div>
-            <h1> תפריט</h1>
+           <h1>תפריט</h1>
             <hr />
         <br></br>
         <div className='flex-containerh'>
-        <div ><button className='button' > <span><b>תיק תלמיד</b></span></button></div>
-        <div ><button className='button' ><span><b>תיק כיתה</b></span></button></div>
-        <div ><button className='button' ><span><b>תיק מורה</b></span></button></div>
+        <div ><button className='button' > <span>תיק תלמיד</span></button></div>
+        <div ><button className='button' ><span>תיק כיתה</span></button></div>
+        <div ><button className='button' ><span>תיק מורה</span></button></div>
         </div>
         </div>
       </div>
