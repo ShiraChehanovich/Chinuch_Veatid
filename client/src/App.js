3789d6ca021e0cb371c942dfb490bbdefae01967
borochov <yael8488274@gmail.com>
2022-05-21 20:18:45 +0300

    Upload documents and functions to tables

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
+<<<<<<< Updated upstream
 import  Signup  from './components/Signup'
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import StudentFile from './components/StudentFile';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from './components/context/AuthContext';
+=======
+// import  Signup  from './components/Signup'
+// import { Container } from 'react-bootstrap';
+// import { AuthProvider } from './components/context/AuthContext';
+// import { Student_table } from './components/context/AuthContext';
+import { ReactFirebaseFileUpload } from './components/ReactFirebaseFileUpload';
+>>>>>>> Stashed changes
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import ForgotPassword from './components/ForgotPassword';
@@ -17,6 +25,7 @@ import StaffTable from './components/StaffTable';
 
 function App() {
   return (
+<<<<<<< Updated upstream
       <Container className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" ,alignContent: 'center'}}>
           <div className="w-100" style={{ maxWidth: "400px" }}>
@@ -51,6 +60,32 @@ function App() {
 //     </Student_table>
 //   );
 // }
+=======
+    <ReactFirebaseFileUpload />
+    // <AuthProvider>
+    // <Container className="d-flex align-items-center justify-content-center"
+    // style={{ minHeight: "100vh" }}>
+    //     <div className="w-100" style={{ maxWidth: "400px" }}>
+    //         <Signup /> 
+    //     </div>
+    // </Container>
+    // </AuthProvider>
+  );
+}
+//function App() {
+  //return (
+    //<Student_table>
+    //</Student_table>/<Container className="d-flex align-items-center justify-content-center"
+    //style={{ minHeight: "100vh" }}>
+        //<div className="w-100" style={{ maxWidth: "400px" }}>
+        //    <Signup /> 
+      //  </div>
+    //</Container>
+   // </Student_table>
+ // );
+//}
+>>>>>>> Stashed changes
 
 
 export default App;
+
