8ec98a5d146560a04bdbc1e3ab9d72bc6b84de9c
Hodaya <kalihodaya@gmail.com>
2022-05-21 23:02:17 +0300

    Textbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
+<<<<<<< HEAD
+=======
+
+import  Signup  from './components/Signup'
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import StudentFile from './components/StudentFile';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from './components/context/AuthContext';
+
+>>>>>>> main
 // import  Signup  from './components/Signup'
 // import { Container } from 'react-bootstrap';
 // import { AuthProvider } from './components/context/AuthContext';
 // import { Student_table } from './components/context/AuthContext';
+<<<<<<< HEAD
 import { ReactFirebaseFileUpload } from './components/ReactFirebaseFileUpload';
+=======
+// import { ReactFirebaseFileUpload } from './components/ReactFirebaseFileUpload';
+
+>>>>>>> main
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import ForgotPassword from './components/ForgotPassword';
@@ -16,7 +32,47 @@ import StaffTable from './components/StaffTable';
 
 function App() {
   return (
+<<<<<<< HEAD
     <ReactFirebaseFileUpload />
+=======
+
+      <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" ,alignContent: 'center'}}>
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Router>
+              <AuthProvider>
+                <Routes>
+                <Route exact path="/" element={<Login />}/>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/students" element={<StudentTable />} />
+                <Route path="/staff" element={<StaffTable />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/studentFile" element={<StudentFile />} />
+                </Routes>
+              </AuthProvider>
+            </Router>
+          </div>
+      </Container>
+  );
+}
+// function App() {
+//   return (
+//     <Student_table>
+//     <Container className="d-flex align-items-center justify-content-center"
+//     style={{ minHeight: "100vh" }}>
+//         <div className="w-100" style={{ maxWidth: "400px" }}>
+//             <Signup /> 
+//         </div>
+//     </Container>
+//     </Student_table>
+//   );
+// }
+
+    /////<ReactFirebaseFileUpload />
+>>>>>>> main
     // <AuthProvider>
     // <Container className="d-flex align-items-center justify-content-center"
     // style={{ minHeight: "100vh" }}>
@@ -25,8 +81,8 @@ function App() {
     //     </div>
     // </Container>
     // </AuthProvider>
-  );
-}
+  //);
+//}
 //function App() {
   //return (
     //<Student_table>
@@ -39,6 +95,10 @@ function App() {
    // </Student_table>
  // );
 //}
+<<<<<<< HEAD
+=======
+
+>>>>>>> main
 
 
 export default App;
