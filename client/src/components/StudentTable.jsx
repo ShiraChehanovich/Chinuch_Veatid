97232d48fecb4c58051d5e83e275e537b913915f
Hodaya <kalihodaya@gmail.com>
2022-05-22 13:33:35 +0300

    search

--- client/src/components/StudentTable.jsx
+++ client/src/components/StudentTable.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PopingMenu from './PopingMenu'
 import Table from './Table'
+import Search from './Search'
 
 export default function StudentTable() {
   return (
     <div>
+      <Search/>
       <PopingMenu/>
       <Table/>
     </div>
