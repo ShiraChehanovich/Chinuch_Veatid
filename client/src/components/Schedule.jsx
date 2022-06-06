55a1788a5e869079564a5eb3f60c3d1d972dccd4
borochov <yael8488274@gmail.com>
2022-06-06 14:52:11 +0300

    date table

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import PopingMenu from './PopingMenu'
+import * as React from 'react';
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import TaskModalPage from './TaskModalPge';
 
-export default function Schedule() {
+export default function StaticDatePickerLandscape() {
+  const [value, setValue] = React.useState(new Date());
+
   return (
-    <div>
-        <PopingMenu/>
-        <h1>העמוד בבניה....</h1>
-        <h3>אנא העזרו בסבלנות</h3>
-    </div>
-  )
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker
+        orientation="landscape"
+        openTo="day"
+        value={value}
+        shouldDisableDate={isWeekend}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      <TaskModalPage/>
+    </LocalizationProvider>
+  );
 }
