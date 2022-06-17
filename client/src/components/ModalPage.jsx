907bd6f4310fbcda74a230e5101ee56be4353a65
Rivka Elgozy <rivkaelgozy@gamil.com>
2022-06-17 16:41:35 +0300

    grade

--- client/src/components/ModalPage.jsx
+++ client/src/components/ModalPage.jsx
@@ -57,6 +57,7 @@ export default function ModalPage(t) {
   const roleRef = React.useRef ()
   const addressRef = React.useRef()
   const gradeRef = React.useRef()
+  const gradeStaffRef = React.useRef()
   const  childRef = React.useRef()
   const navigate = useNavigate()
   const { currentUser,  } = useAuth()
@@ -146,6 +147,9 @@ export default function ModalPage(t) {
       window.location.reload(false);
     }
     const handleSubmitStaff = async () =>{
+      // console.log("seted to: "+ gradeRef.current)
+      var array = ((gradeRef.current).toString()).split(' ')
+      console.log( array)
       // e.preventDefault()
     await setDoc(doc(collection(firestore, "staff" )),{
       // name: newName,
@@ -158,8 +162,13 @@ export default function ModalPage(t) {
       name: fnameRef.current.value,
       lastName: lNameRef.current.value,
       idUser: parseInt(IDRef.current.value, 10),
-      grade: childRef.current.value,
-      email: emailRef.current.value,
+      // grade.forEach(element => {
+      //   grade[i]:gradeStaffRef.current,
+      //   i++
+        
+      // });
+      grade: array.current,
+      email: emailRef.current,
       phone: phoneRef.current.value,  
       role: roleRef.current.value,
      address: addressRef.current.value,
@@ -214,7 +223,7 @@ export default function ModalPage(t) {
                 <Form.Group id="grade">
                 {/* <RegularTextField t = "כיתה"  value = {newGrade onChange={(event) => setNewAge(event.target.value)}></RegularTextField> */}
                 {tableType.tableType == 'Staff' ? null: (<Form.Label>כיתה</Form.Label>)}
-                {tableType.tableType == 'Staff' ? <SelectedRolingList ></SelectedRolingList> : (<Form.Control type="text" ref={gradeRef} required />)}
+                {tableType.tableType == 'Staff' ? <SelectedRolingList handleChange={(grade) => {gradeRef.current = grade; console.log("change to:  " + gradeRef.current )}} ></SelectedRolingList> : (<Form.Control type="text" ref={gradeRef} required />)}
                 </Form.Group>
                 <Form.Group id="phone">
                 {/* <RegularTextField t = "טלפון"  value = {newPhone} onChange={(event) => setNewPhone(event.target.value)}></RegularTextField> */}
